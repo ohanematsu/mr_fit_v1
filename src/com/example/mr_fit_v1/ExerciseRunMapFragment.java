44e7af2015eb73e2e05c6b4c25b90400c9d9ed4b
Jiawen Zeng <zengjw1990@gmail.com>
2014-04-20 23:12:34 -0400

    Fix typo

--- src/com/example/mr_fit_v1/ExerciseRunMapFragment.java
+++ src/com/example/mr_fit_v1/ExerciseRunMapFragment.java
@@ -1,5 +1,7 @@
 package com.example.mr_fit_v1;
 
-public class ExerciseRunMapFragment extends SupportMa{
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class ExerciseRunMapFragment extends SupportMapFragment{
 
 }
