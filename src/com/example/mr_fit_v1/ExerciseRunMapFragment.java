9dd1c328d90b2ccb3a5d8026e64045566103e507
Xiaolin <xiaolinyuan06@gmail.com>
2014-04-21 21:03:54 -0400

    deleted used

--- src/com/example/mr_fit_v1/ExerciseRunMapFragment.java
+++ src/com/example/mr_fit_v1/ExerciseRunMapFragment.java
@@ -1,7 +1,7 @@
 package com.example.mr_fit_v1;
 
-import com.google.android.gms.maps.SupportMapFragment;
 
-public class ExerciseRunMapFragment extends SupportMapFragment{
+
+public class ExerciseRunMapFragment {
 
 }
