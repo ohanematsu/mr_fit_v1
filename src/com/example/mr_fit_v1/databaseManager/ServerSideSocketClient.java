a6afa6ebff7c37838ff271206b5a6391fe84d937
Xiaolin <xiaolinyuan06@gmail.com>
2014-05-02 21:24:55 -0400

    aaa

---
+++ src/com/example/mr_fit_v1/databaseManager/ServerSideSocketClient.java
@@ -0,0 +1,17 @@
+package com.example.mr_fit_v1.databaseManager;
+
+public class ServerSideSocketClient {
+
+	public ServerSideSocketClient() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
